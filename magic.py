4f86256740e40c85bec0c9324a0583ccbb3fc6a5
abuginpython <abuginpython>
2024-01-20 13:57:44 +0800

    mcgato

---
+++ magic.py
@@ -0,0 +1,3 @@
+import random as rnd
+
+print(f'Mcgato is {rnd.randint(0,100)}% Gay!')
---
+++ mcgato.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+import ttkbootstrap as ttk 
+
+wind = tk.Tk()
+#hmmmmmmm
+wild = ttk.Label(text = "Wild", font = "Papyrus 100 bold")
+a = ttk.Label(text = "Femboy coding #1")
+wild.pack()
+a.pack(side="left")
+wind.mainloop()
